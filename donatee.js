49f75d4bf9d73cf145d9a42c33d9b7b2b7d4c83f
palash0111 <aymanislammayan@gmail.com>
2024-09-23 05:02:46 +0600

    finally assignment done

--- donatee.js
+++ donatee.js
@@ -2,51 +2,7 @@ let totalAmount = 5500;
 let addMoney = 0;
 let addMoney1 = 0;
 let addMoney2 = 0;
-document.getElementById('success-modal').addEventListener('click', function(){
 
-    const p = document.createElement('p');
-    p.style.backgroundColor = 'white';
-    p.style.border = '2px solid gray';
-    p.style.padding = '10px';
-    p.style.textAlign = 'center';
-    p.style.width = '700px';
-    p.style.marginTop = '30px';
-    p.style.marginLeft = '300px'
-    p.innerText = `96500 Taka is Donated for famine-2024 at Feni, Bangladesh `;
-    const transactionContainer =document.getElementById('history-form');
-    transactionContainer.appendChild(p);
-
-})
-document.getElementById('success-modal1').addEventListener('click', function(){
-
-    const p = document.createElement('p');
-    p.style.backgroundColor = 'white';
-    p.style.border = '2px solid gray';
-    p.style.padding = '10px';
-    p.style.textAlign = 'center';
-    p.style.width = '700px';
-    p.style.marginTop = '30px';
-    p.style.marginLeft = '300px'
-    p.innerText = `96500 Taka is Donated for famine-2024 at Feni, Bangladesh `;
-    const transactionContainer =document.getElementById('history-form');
-    transactionContainer.appendChild(p);
-
-})
-document.getElementById('success-modal2').addEventListener('click', function(){
-
-    const p = document.createElement('p');
-    p.style.backgroundColor = 'white';
-    p.style.border = '2px solid gray';
-    p.style.padding = '10px';
-    p.style.textAlign = 'center';
-    p.style.width = '700px';
-    p.style.marginTop = '30px';
-    p.style.marginLeft = '300px'
-    p.innerText = `Donated`
-    const transactionContainer =document.getElementById('history-form');
-    transactionContainer.appendChild(p);
-
-})
 document.getElementById('close-modal').addEventListener('click', function(){
     const amount = getInputValueById('donation-amount1');
 let total = getTextValueById('total-amount')
@@ -54,6 +10,19 @@ totalAmount -=amount;
 addMoney +=  amount;
 document.getElementById('add-money').innerText = addMoney;
 document.getElementById('total-amount').innerText = totalAmount;
+
+
+const p = document.createElement('p');
+p.style.backgroundColor = 'white';
+p.style.border = '2px solid gray';
+p.style.padding = '10px';
+p.style.textAlign = 'center';
+p.style.width = '700px';
+p.style.marginTop = '30px';
+p.style.marginLeft = '300px'
+p.innerText = `${amount} Taka is Donated for famine-2024 at Feni, Bangladesh `;
+const transactionContainer =document.getElementById('history-form');
+transactionContainer.appendChild(p);
 
     if (isNaN(amount)) {
         alert('Please enter a valid donation amount')
@@ -80,6 +49,20 @@ addMoney1 +=  amount2;
 document.getElementById('add-money2').innerText = addMoney1;
 document.getElementById('total-amount').innerText = totalAmount;
 
+
+
+const p = document.createElement('p');
+p.style.backgroundColor = 'white';
+p.style.border = '2px solid gray';
+p.style.padding = '10px';
+p.style.textAlign = 'center';
+p.style.width = '700px';
+p.style.marginTop = '30px';
+p.style.marginLeft = '300px'
+p.innerText = `${amount2}  Taka is Donated for Flood Relief in Feni,Bangladesh `;
+const transactionContainer =document.getElementById('history-form');
+transactionContainer.appendChild(p);
+
     if (isNaN(amount2)) {
         alert('Please enter a valid donation amount')
         return;
@@ -104,6 +87,19 @@ totalAmount -=amount3;
 addMoney2 +=  amount3;
 document.getElementById('add-money3').innerText = addMoney2;
 document.getElementById('total-amount').innerText = totalAmount;
+
+
+const p = document.createElement('p');
+p.style.backgroundColor = 'white';
+p.style.border = '2px solid gray';
+p.style.padding = '10px';
+p.style.textAlign = 'center';
+p.style.width = '700px';
+p.style.marginTop = '30px';
+p.style.marginLeft = '300px'
+p.innerText = `${amount3}Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh `;
+const transactionContainer =document.getElementById('history-form');
+transactionContainer.appendChild(p);
 
     if (isNaN(amount3)) {
         alert('Please enter a valid donation amount')
