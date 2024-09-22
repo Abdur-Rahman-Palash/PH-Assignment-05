97f4d11aefe5849db13e5b2cb0e63f7168351f51
palash0111 <aymanislammayan@gmail.com>
2024-09-23 04:10:50 +0600

    JavaScript finished

--- donatee.js
+++ donatee.js
@@ -1,13 +1,123 @@
-document.getElementById('donate-now1').addEventListener('click', function(event){
-    event.preventDefault();
-const donationAmount1 = getInputValueById('donation-amount1');
-    if(donationAmount1 === 123){
-        const totalBalance = getTextValueById('total-amount');
-        const totalAmountRemaining = totalBalance - donationAmount1;
-        document.getElementById('total-amount').innerText = totalAmountRemaining;
-    }
-if(isNaN(donationAmount1) && totalBalance < donationAmount1){
-    alert('Failed transaction and insufficient fund')
-    return;
-}
+let totalAmount = 5500;
+let addMoney = 0;
+let addMoney1 = 0;
+let addMoney2 = 0;
+document.getElementById('success-modal').addEventListener('click', function(){
+
+    const p = document.createElement('p');
+    p.style.backgroundColor = 'white';
+    p.style.border = '2px solid gray';
+    p.style.padding = '10px';
+    p.style.textAlign = 'center';
+    p.style.width = '700px';
+    p.style.marginTop = '30px';
+    p.style.marginLeft = '300px'
+    p.innerText = `96500 Taka is Donated for famine-2024 at Feni, Bangladesh `;
+    const transactionContainer =document.getElementById('history-form');
+    transactionContainer.appendChild(p);
+
+})
+document.getElementById('success-modal1').addEventListener('click', function(){
+
+    const p = document.createElement('p');
+    p.style.backgroundColor = 'white';
+    p.style.border = '2px solid gray';
+    p.style.padding = '10px';
+    p.style.textAlign = 'center';
+    p.style.width = '700px';
+    p.style.marginTop = '30px';
+    p.style.marginLeft = '300px'
+    p.innerText = `96500 Taka is Donated for famine-2024 at Feni, Bangladesh `;
+    const transactionContainer =document.getElementById('history-form');
+    transactionContainer.appendChild(p);
+
+})
+document.getElementById('success-modal2').addEventListener('click', function(){
+
+    const p = document.createElement('p');
+    p.style.backgroundColor = 'white';
+    p.style.border = '2px solid gray';
+    p.style.padding = '10px';
+    p.style.textAlign = 'center';
+    p.style.width = '700px';
+    p.style.marginTop = '30px';
+    p.style.marginLeft = '300px'
+    p.innerText = `Donated`
+    const transactionContainer =document.getElementById('history-form');
+    transactionContainer.appendChild(p);
+
+})
+document.getElementById('close-modal').addEventListener('click', function(){
+    const amount = getInputValueById('donation-amount1');
+let total = getTextValueById('total-amount')
+totalAmount -=amount;
+addMoney +=  amount;
+document.getElementById('add-money').innerText = addMoney;
+document.getElementById('total-amount').innerText = totalAmount;
+
+    if (isNaN(amount)) {
+        alert('Please enter a valid donation amount')
+        return;
+    
+    } 
+    if (amount > total) {
+        alert('apni balancer cheye besi tk nite chascen,ata hobe na')
+        return;
+    
+    } 
+    if (amount < 0) {
+        alert('insufficient fund')
+        return;
+    
+    }  
+})
+
+document.getElementById('close-modal1').addEventListener('click', function(){
+    const amount2 = getInputValueById('donation-amount2');
+let total1 = getTextValueById('total-amount')
+totalAmount -=amount2;
+addMoney1 +=  amount2;
+document.getElementById('add-money2').innerText = addMoney1;
+document.getElementById('total-amount').innerText = totalAmount;
+
+    if (isNaN(amount2)) {
+        alert('Please enter a valid donation amount')
+        return;
+    
+    } 
+    if (amount2 > total1) {
+        alert('apni balancer cheye besi tk nite chascen,ata hobe na')
+        return;
+    
+    } 
+    if (amount2 < 0) {
+        alert('insufficient fund')
+        return;
+    
+    }  
+})
+
+document.getElementById('close-modal2').addEventListener('click', function(){
+    const amount3 = getInputValueById('donation-amount3');
+let total2 = getTextValueById('total-amount')
+totalAmount -=amount3;
+addMoney2 +=  amount3;
+document.getElementById('add-money3').innerText = addMoney2;
+document.getElementById('total-amount').innerText = totalAmount;
+
+    if (isNaN(amount3)) {
+        alert('Please enter a valid donation amount')
+        return;
+    
+    } 
+    if (amount3 > total2) {
+        alert('apni balancer cheye besi tk nite chascen,ata hobe na')
+        return;
+    
+    } 
+    if (amount3 < 0) {
+        alert('insufficient fund')
+        return;
+    
+    }  
 })
--- toggle.js
+++ toggle.js
@@ -1,6 +1,11 @@
 document.getElementById('donation').addEventListener('click',function(){
-showSectionById('donation1-form')
+showSectionById('donation1-form');
     })
     document.getElementById('history').addEventListener('click',function(){
     showSectionById('history-form')
         })
+document.getElementById('history').addEventListener('click', function(){
+const history = document.getElementById('history')
+history.classList.remove('bg-gray-300', 'text-black');
+history.classList.add('bg-gray-200', 'text-gray-600');
+})
